e996bb6f90af36bc1e8d4e2bef3b83f59796297d
Dilasha Shakya <dilashakya@gmail.com>
2026-01-01 17:26:06 -0600

    tray icon and functionality added to pomodoro timer

--- Pomodoro/timer/public/electron.js
+++ Pomodoro/timer/public/electron.js
@@ -1,9 +1,11 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, Tray, Menu} = require("electron");
 const path = require("path");
 const url = require("url")
+let win;
+let tray = null;
 
 function createWindow (){
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         title: 'WorkEfficiently',
         width:500,
         height: 500,
@@ -22,4 +24,58 @@ function createWindow (){
     win.loadURL(startUrl); //load app in electron window
 }
 
-app.whenReady().then(createWindow)
+
+
+function createTray(){
+    const logo = path.join(__dirname,'logo.png' )
+    tray = new Tray(logo);
+
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Show pomodoro',
+            click: ()=>{
+                if (win) win.show()
+            }
+        },
+        {
+            label: 'Hide',
+            click: ()=>{
+                if (win) win.hide();
+            }
+        },
+        { type: 'separator' },
+        {
+            label: 'Quit',
+            click: ()=>{
+                app.isQuiting = true;
+                app.quit();
+            }
+        }
+    ]);
+
+    tray.setContextMenu(contextMenu)
+    tray.setToolTip('Pomodoro Timer');
+
+    // Double-click tray icon to show/hide window
+    tray.on('double-click', () => {
+        if (win) {
+            if (win.isVisible()) {
+                win.hide();
+            } else {
+                win.show();
+            }
+        }
+    });
+}
+
+app.isQuiting = false;
+
+app.whenReady().then(()=>{
+    createWindow();
+    createTray();
+});
+
+// Prevent window from closing, hide to tray instead
+app.on('before-quit', () => {
+    app.isQuiting = true;
+});
